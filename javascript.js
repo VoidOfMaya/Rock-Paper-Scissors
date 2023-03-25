7967871e32e528a932580d5eb0a3d67c85519b8f
VoidOfMaya <mayaofthevoid@gmail.com>
2023-03-25 21:11:17 +0300

    create random number picker method

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("testing testing");
+// Rock Paper Scissors
+//array of ,rock, paper, scissors,
+//function to choose how many rounds to play max 10 rounds
+//function to randomly pick  any of the 3 options
+//function allows player to choose one of 3 options
+//function that compares and decidses the winner per round
+//function that counts and declares the winner per session
+
+
+let choiceArry = ["rock", "paper", "scissors"];
+
+
+let ComputerChoice = Math.round(Math.random()*3);
+
+console.log(ComputerChoice);
