539e210d3cd108da91feb533709e2f224716b9b9
VoidOfMaya <mayaofthevoid@gmail.com>
2023-03-25 23:32:40 +0300

    add main game logic
    -add main rock paper scissors logic
    -user input still requirs constraints
    -required to build a session round logic
    -required to build a session score count system

--- javascript.js
+++ javascript.js
@@ -5,14 +5,64 @@
 //function allows player to choose one of 3 options
 //function that compares and decidses the winner per round
 //function that counts and declares the winner per session
-
+// logic of the game:
+//-scissors beat paper,
+//-paper beats rock,
+//-rock beats scissors,
 
 let choiceArray = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let userScore = 0;
 
 
-let ComputerChoice = choiceArray[Math.round(Math.random()*2)];
+let computerChoice = choiceArray[Math.round(Math.random()*2)];
 
 
 let userChoice = prompt ("write to console one of the following choises:\n rock / paper / scissors").toLowerCase();
 
-console.log(userChoice);
+
+function playRound(playerSelection, computerSelection){
+    switch(playerSelection){
+        case "rock":
+            if(computerSelection == playerSelection){
+                console.log("tie!");
+            }
+            else if(computerSelection == "paper"){
+                console.log("Computer wins!")
+                return computerScore + 1;
+            }
+            else if(computerSelection == "scissors"){
+                console.log("You win");
+                return userScore + 1;
+            }
+            break;
+        case "paper":
+            if(computerSelection == playerSelection){
+                console.log("tie!");
+            }
+            else if(computerSelection == "rock"){
+                console.log("You win!")
+                return userScore + 1;
+            }
+            else if(computerSelection == "scissors"){
+                console.log("Computer wins!");
+                return computerScore + 1;
+            }
+            break;
+        case "scissors":
+            if(computerSelection == playerSelection){
+                console.log("tie!");
+            }
+            else if(computerSelection == "paper"){
+                console.log("You wins!")
+                return userScore + 1;
+            }
+            else if(computerSelection == "rock"){
+                console.log("Computer win");
+                return computerScore + 1;
+            }
+            break;
+        default:
+    }
+}
+
