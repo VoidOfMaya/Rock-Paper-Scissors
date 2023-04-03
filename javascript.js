ce2f2ee4ae0b65a6609ffe8833b76977830962c7
VoidOfMaya <mayaofthevoid@gmail.com>
2023-04-03 21:46:02 +0300

    restructuring program toa compidate button press
    -removal of loop on main logic
    -realocation of functions  outside  of playRound to maintain
    clean code and keep readable
    -alocating loop to game function

--- javascript.js
+++ javascript.js
@@ -1,80 +1,94 @@
 // Rock Paper Scissors
-//array of ,rock, paper, scissors,
-//function to choose how many rounds to play max 10 rounds
-//function to randomly pick  any of the 3 options
-//function allows player to choose one of 3 options
+//variable array of ,rock, paper, scissors,
+//variable to choose how many rounds to play max 10 rounds
+//variable to randomly pick  any of the 3 options
+//input variable asks player to choose one of 3 options
 //function that compares and decidses the winner per round
 //function that counts and declares the winner per session
+
 // logic of the game:
 //-scissors beat paper,
 //-paper beats rock,
 //-rock beats scissors,
 
 
-// array for game options index positons are 0=rock , 1=paper , 2=scissors
 let choiceArray = ["rock", "paper", "scissors"];
+// randomizer ai
+function computerChoice(){
+
+    let computerSelection = choiceArray[Math.round(Math.random()*2)];    
+    return computerSelection;
+    };
+
+// player choiceS
+function playerChoice(){
+    const buttons = this.document.querySelectorAll('button');
+    buttons.forEach((btn) => btn.addEventListener('click', () =>{
+        playRound(btn.id, computerChoice());
+    }));
 
 
-// handles main gamelogic + keeping score
-function playRound(sessionNumber){
-
-    let scoreCount=0;
-
-   
-    for (let i = 0; i< sessionNumber; i++) {
-
-        let computerSelection = choiceArray[Math.round(Math.random()*2)];
-        let playerSelection = prompt ("write to console one of the following choises:\n rock / paper / scissors").toLowerCase();
-        let resultText = `computer chose ${computerSelection} , you chose ${playerSelection} `;  
-        
-        switch(playerSelection){
-            case choiceArray[0]:
-                if(computerSelection == playerSelection){
-                    console.log(`${resultText}its a tie!`);
-                }
-                else if(computerSelection == choiceArray[1]){
-                    console.log(`${resultText} Computer win!`)
-                    scoreCount--;
-                }
-                else if(computerSelection == choiceArray[2]){
-                    console.log(`${resultText} You win`);
-                    scoreCount++;
-                }
-                break;
-            case choiceArray[1]:
-                if(computerSelection == playerSelection){
-                    console.log(`${resultText} its a tie!`);
-                }
-                else if(computerSelection == choiceArray[0]){
-                    console.log(`${resultText} You win!`)
-                    scoreCount++;
-                }
-                else if(computerSelection == choiceArray[2]){
-                    console.log(`${resultText} Computer wins!`);
-                    scoreCount--;
-                }
-                break;
-            case choiceArray[2]:
-                if(computerSelection == playerSelection){
-                    console.log(`${resultText} its s tie!`);
-                }
-                else if(computerSelection == choiceArray[1]){
-                    console.log(`${resultText} You wins!`)
-                    scoreCount++;
-                }
-                else if(computerSelection == choiceArray[0]){
-                    console.log(`${resultText} Computer win`);
-                    scoreCount--;
-                }
-                break;
-            default:
-        }
-    }
-    return scoreCount;
 
 }
 
-//handles calculatin the winner
+
+
+// handles main gamelogic + keeping score by returning a number  
+function playRound(playerInput , aiInput){
+    let scoreCount =0;
+    let computerSelection = aiInput;
+    let playerSelection = playerInput; 
+
+    let resultText = `computer chose ${computerSelection} , you chose ${playerSelection} `; 
+    switch(playerSelection){
+        case choiceArray[0]:
+            if(computerSelection == playerSelection){
+                console.log(`${resultText}its a tie!`);
+            }
+            else if(computerSelection == choiceArray[1]){
+                console.log(`${resultText} Computer win!`)
+                scoreCount--;
+            }
+            else if(computerSelection == choiceArray[2]){
+                console.log(`${resultText} You win`);
+                scoreCount++;
+            }
+            break;
+        case choiceArray[1]:
+            if(computerSelection == playerSelection){
+                console.log(`${resultText} its a tie!`);
+            }
+            else if(computerSelection == choiceArray[0]){
+                console.log(`${resultText} You win!`)
+                scoreCount++;
+            }
+            else if(computerSelection == choiceArray[2]){
+                console.log(`${resultText} Computer wins!`);
+                scoreCount--;
+            }
+            break;
+        case choiceArray[2]:
+            if(computerSelection == playerSelection){
+                console.log(`${resultText} its s tie!`);
+            }
+            else if(computerSelection == choiceArray[1]){
+                console.log(`${resultText} You wins!`)
+                scoreCount++;
+            }
+            else if(computerSelection == choiceArray[0]){
+                console.log(`${resultText} Computer win`);
+                scoreCount--;
+            }
+            break;
+        default:
+    }
+
+    return scoreCount;
+
+
+}
+
+//handles calculatin the winner using a recived number
 function sessionWiner(numb){
     if (gameResult == 0){
         console.log("this sessioin is a tie");
@@ -87,10 +101,10 @@ function sessionWiner(numb){
 
 //main game function that launches all game related function
 function game (){
-    let rounds = prompt("type in how many rounds youd like to play min is 3 rounds max is 10 rounds");
-    gameResult = playRound(rounds);
-    sessionWiner(gameResult);
- 
-}
 
+    for(let i=0; i < 3; i++){
+      playerChoice();    
+    }
+
+}
 game();
